2317af012c48985a79e630634dae4522759bb7b2
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-08-01 10:38:06 +0200

    Donation entity created

--- src/main/java/pl/coderslab/charity/donation/Donation.java
+++ src/main/java/pl/coderslab/charity/donation/Donation.java
@@ -5,16 +5,22 @@ import lombok.Setter;
 import pl.coderslab.charity.category.Category;
 import pl.coderslab.charity.institution.Institution;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 @Setter
 @Getter
+@Entity
 public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Integer quantity;
+    @ManyToMany
     private List<Category> categories;
+    @ManyToOne
     private Institution institution;
     private String street;
     private String city;
